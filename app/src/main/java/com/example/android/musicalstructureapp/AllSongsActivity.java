c679adbaedfa73372b431effffcaa0d0edc6f14d
kboul <kos.voulgaridis@gmail.com>
2017-05-08 00:31:58 +0300

    Add now playing activity & content

--- app/src/main/java/com/example/android/musicalstructureapp/AllSongsActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/AllSongsActivity.java
@@ -1,13 +1,36 @@
 package com.example.android.musicalstructureapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class AllSongsActivity extends AppCompatActivity {
+
+    int[] nowPlayingIds = new int[]{
+            R.id.nowPlaying1,
+            R.id.nowPlaying2,
+            R.id.nowPlaying3,
+            R.id.nowPlaying4
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_songs);
+
+        //Transfer the user to the Now Playing Activity when selecting one of the songs
+        for (int i = 0; i < nowPlayingIds.length; i++) {
+            TextView nowPlaying = (TextView) findViewById(nowPlayingIds[i]);
+            nowPlaying.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent nowPlayingIntent = new Intent(AllSongsActivity.this, NowPlayingActivity.class);
+                    startActivity(nowPlayingIntent);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
@@ -3,11 +3,11 @@ package com.example.android.musicalstructureapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class PlaylistsActivity extends AppCompatActivity {
+public class NowPlayingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_playlists);
+        setContentView(R.layout.activity_now_playing);
     }
 }
